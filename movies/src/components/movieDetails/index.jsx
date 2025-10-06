f8ae746dea882e6a034b97ffb262417d58aea7ea
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-10-06 13:39:58 +0100

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -51,6 +51,8 @@ const MovieDetails = ({ movie }) => {
                     </li>
                 ))}
             </Paper>
+
+
             <Paper component="ul" sx={{ ...root }}>
                 <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
                 <Chip
@@ -63,6 +65,20 @@ const MovieDetails = ({ movie }) => {
                 />
                 <Chip label={`Released: ${movie.release_date}`} />
             </Paper>
+
+             {movie.production_countries && movie.production_countries.length > 0 && (
+                <Paper component="ul" sx={{ ...root }}>
+                    <li>
+                        <Chip label="Production Countries" sx={{ ...chip }} color="primary" />
+                    </li>
+                    {movie.production_countries.map((c) => (
+                        <li key={c.name}>
+                            <Chip label={c.name} sx={{ ...chip }} />
+                        </li>
+                    ))}
+                </Paper>
+            )}
+            
             <Fab
                 color="secondary"
                 variant="extended"
