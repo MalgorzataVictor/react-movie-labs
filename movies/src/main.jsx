f81b38c67ee9d4672e0433df7a7af061a477fa22
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-10-06 13:02:55 +0100

    Full review page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
@@ -20,11 +22,13 @@ const App = () => {
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
+
       </Routes>
     </BrowserRouter>
   );
 };
 
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot(document.getElementById("root"))
 rootElement.render(<App />);
