f79252fcd9e73e803780110c589db67a2b9398b6
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-10-13 15:06:02 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -3,8 +3,7 @@ import { getUpcomingMovie } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
-
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
 
 const UpcomingMoviePage = (props) => {
 
@@ -27,7 +26,11 @@ const UpcomingMoviePage = (props) => {
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+      action={(movie) => {
+        return (
+            <PlaylistAddIcon color="primary" fontSize="large" />
+        );
+      }}
     />
 
   );
