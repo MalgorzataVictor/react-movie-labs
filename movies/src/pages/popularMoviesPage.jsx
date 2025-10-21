723434253a907e22407230d3c5ae94ff8d1edbec
Malgorzata Victor <malgorzatavictor@gmail.com>
2025-10-21 22:58:37 +0100

    Add pagination as reuseable component

--- movies/src/pages/popularMoviesPage.jsx
+++ movies/src/pages/popularMoviesPage.jsx
@@ -3,7 +3,9 @@ import { getPopularMovie } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
-import { Box, Pagination } from "@mui/material";
+import { Box } from "@mui/material";
+import Pagination from "../components/pagination";
+
 
 
 const PopularMoviePage = (props) => {
@@ -35,24 +37,8 @@ const PopularMoviePage = (props) => {
 
 			<Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
 				<Pagination
-					count={10}
 					page={page}
 					onChange={(e, value) => setPage(value)}
-					size="large"
-					sx={{
-						"& .MuiPaginationItem-root": {
-							fontWeight: "bold",        
-							fontSize: "1.5rem",        
-						},
-						"& .Mui-selected": {
-							backgroundColor: "#cc0000", 
-							color: "white",
-							fontWeight: "bold",
-						},
-						"& .MuiPaginationItem-root:hover": {
-							backgroundColor: "rgba(204,0,0,0.3)", 
-						},
-					}}
 				/>
 			</Box>
 		</Box>
